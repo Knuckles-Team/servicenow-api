5e9c06c43f4f3539aa7199bc4a90131db058f684
Audel Rouhi <Audel_Rouhi@Dell.com>
2022-11-18 15:10:51 -0600

    Initial Commit

---
+++ servicenow_api/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+from servicenow_api.version import __version__, __author__, __credits__
+from servicenow_api.servicenow_api import Api
+
+"""
+ServiceNow API
+
+A Python Wrapper for ServiceNow API
+"""
+
+__version__ = __version__
+__author__ = __author__
+__credits__ = __credits__
---
+++ servicenow_api/version.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+__version__ = "0.0.1"
+__author__ = 'Audel Rouhi'
+__credits__ = 'Audel Rouhi'
