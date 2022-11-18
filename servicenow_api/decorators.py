fceb8d2c235c068f61457f0c2d271697e67484d4
Audel Rouhi <Audel_Rouhi@Dell.com>
2022-11-18 16:15:42 -0600

    reformated file.

--- servicenow_api/decorators.py
+++ servicenow_api/decorators.py
@@ -2,19 +2,23 @@
 # coding: utf-8
 
 import functools
+
 try:
     from servicenow_api.exceptions import LoginRequiredError
 except ModuleNotFoundError:
     from exceptions import LoginRequiredError
+
 
 def require_auth(function):
     """
     Wraps API calls in function that ensures headers are passed
     with a token
     """
+
     @functools.wraps(function)
     def wrapper(self, *args, **kwargs):
         if not self.headers:
             raise LoginRequiredError
         return function(self, *args, **kwargs)
+
     return wrapper
