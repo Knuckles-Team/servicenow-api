fe251719c3aa46a9e432357d55c32527fcdda85c
Audel Rouhi <knucklessg1@gmail.com>
2022-11-21 13:05:01 -0600

    Incremented version as well.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ServiceNow API
-*Version: 0.0.1*
+*Version: 0.0.2*
 
 ServiceNow API Python Wrapper
 
--- servicenow_api/version.py
+++ servicenow_api/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = 'Audel Rouhi'
 __credits__ = 'Audel Rouhi'
