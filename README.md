8d577355f564d72ae769b7e2a29fc5850615d216
GitHub Pipeline <GitHub@users.noreply.github.com>
2022-11-29 20:35:11 +0000

    Automated Version Increment to 0.4.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ServiceNow API
-*Version: 0.3.0*
+*Version: 0.4.0*
 
 ServiceNow API Python Wrapper
 
--- servicenow_api/version.py
+++ servicenow_api/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __author__ = 'Audel Rouhi'
 __credits__ = 'Audel Rouhi'
