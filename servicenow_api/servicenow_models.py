5b6e6232799020714da8062c47614976db1b8709
Audel Rouhi <knucklessg1@gmail.com>
2024-05-21 09:43:21 -0500

    Pre-commit.

--- servicenow_api/servicenow_models.py
+++ servicenow_api/servicenow_models.py
@@ -2067,8 +2067,12 @@ class Response(BaseModel):
         default=None, description="List of services related to the identified service."
     )
     error: Optional[Any] = None
-    status_code: Union[str, int] = Field(default=None, description="Response status code")
-    json: Optional[Union[List, Dict]] = Field(default=None, description="Response JSON data")
+    status_code: Union[str, int] = Field(
+        default=None, description="Response status code"
+    )
+    json: Optional[Union[List, Dict]] = Field(
+        default=None, description="Response JSON data"
+    )
     raw: Optional[bytes] = Field(default=None, description="Response Raw bytes")
 
     @field_validator("service")
