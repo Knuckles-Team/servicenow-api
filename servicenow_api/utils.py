e7e9503b26309a6706a901253173e670451d2c9c
knucklessg1 <knucklessg1@gmail.com>
2024-05-21 09:50:54 -0500

    Fixed outputs.

--- servicenow_api/utils.py
+++ servicenow_api/utils.py
@@ -20,13 +20,18 @@ def process_response(response: requests.Response) -> Union[Response, requests.Re
     except Exception as response_error:
         print(f"Response Error: {response_error}")
     status_code = response.status_code
-    raw = response.content
+    raw_output = response.content
     try:
         response = response.json()
     except Exception as response_error:
         print(f"JSON Conversion Error: {response_error}")
     try:
-        response = Response(**response, status_code=status_code, raw=raw, json=response)
+        response = Response(
+            **response,
+            status_code=status_code,
+            raw_output=raw_output,
+            json_output=response,
+        )
     except Exception as response_error:
         print(f"Response Model Application Error: {response_error}")
 
