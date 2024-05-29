8a7e1a9c8dfccf96d3fe1a9556393498188c79e6
Audel Rouhi <knucklessg1@gmail.com>
2024-05-28 22:14:05 -0500

    Added logging to api.

--- servicenow_api/utils.py
+++ servicenow_api/utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
+import logging
 from typing import Union
 
 import requests
@@ -12,19 +13,22 @@ except ModuleNotFoundError:
     from servicenow_models import (
         Response,
     )
+logging.basicConfig(
+    level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def process_response(response: requests.Response) -> Union[Response, requests.Response]:
     try:
         response.raise_for_status()
     except Exception as response_error:
-        print(f"Response Error: {response_error}")
+        logging.error(f"Response Error: {response_error}")
     status_code = response.status_code
     raw_output = response.content
     try:
         response = response.json()
     except Exception as response_error:
-        print(f"JSON Conversion Error: {response_error}")
+        logging.error(f"JSON Conversion Error: {response_error}")
     try:
         response = Response(
             **response,
@@ -33,6 +37,6 @@ def process_response(response: requests.Response) -> Union[Response, requests.Re
             json_output=response,
         )
     except Exception as response_error:
-        print(f"Response Model Application Error: {response_error}")
+        logging.error(f"Response Model Application Error: {response_error}")
 
     return response
