5d6d6ce7a1d80129551da5d86bdea8264060b59b
KnucklesSG1 <knucklessg1@gmail.com>
2025-10-03 01:26:38 -0500

    Import all funcs in init.

--- servicenow_api/__init__.py
+++ servicenow_api/__init__.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from servicenow_api.servicenow_api import Api
-from servicenow_api.servicenow_api_mcp import servicenow_api_mcp
-from servicenow_api.servicenow_models import (
-    ApplicationServiceModel,
-    CMDBModel,
-    CICDModel,
-    ChangeManagementModel,
-    IncidentModel,
-    ImportSetModel,
-    TableModel,
-)
+import importlib
+import inspect
+
+# List of modules to import from
+MODULES = [
+    "servicenow_api.servicenow_api",
+    "servicenow_api.servicenow_api_mcp",
+    "servicenow_api.servicenow_models",
+    "servicenow_api.decorators",
+    "servicenow_api.exceptions",
+    "servicenow_api.utils",
+]
+
+# Initialize __all__ to expose all public classes and functions
+__all__ = []
+
+# Dynamically import all classes and functions from the specified modules
+for module_name in MODULES:
+    module = importlib.import_module(module_name)
+    for name, obj in inspect.getmembers(module):
+        # Include only classes and functions, excluding private (starting with '_')
+        if (inspect.isclass(obj) or inspect.isfunction(obj)) and not name.startswith(
+            "_"
+        ):
+            globals()[name] = obj
+            __all__.append(name)
 
 """
 ServiceNow API
 
 A Python Wrapper for ServiceNow API
 """
-
-__all__ = [
-    "Api",
-    "servicenow_api_mcp",
-    "ApplicationServiceModel",
-    "CMDBModel",
-    "CICDModel",
-    "ChangeManagementModel",
-    "ChangeManagementModel",
-    "IncidentModel",
-    "ImportSetModel",
-    "TableModel",
-]
